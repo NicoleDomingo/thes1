b8a9a3cf85300826dbc9de3e385b96eb9d95c8f4
jayveegabriel <jaybriel19@gmail.com>
2018-10-04 17:50:30 +0800

    Try commit

--- thes1/thes1/urls.py
+++ thes1/thes1/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,  include
 from manageBed import views
 
-#hehe
+#hehew
 urlpatterns = [
 	path('', include('manageBed.urls')),
     path('admin/', admin.site.urls),
